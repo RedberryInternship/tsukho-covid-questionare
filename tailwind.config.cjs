979c1af071bbc341394517a81ccea63c860c9f80
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-12-16 00:31:11 +0400

    fix: fix font import

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["BPGArial", "Helvetica", "Arial", "sans-serif"],
+        sans: ["BPGArial"],
       },
       colors: {
         neutral: {
