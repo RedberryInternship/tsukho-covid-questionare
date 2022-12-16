ceb640c906c5061f647fc7b85e17d263925bc63d
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-12-16 15:21:32 +0400

    feat: add first two pages image and animations

--- src/pages/FormPages/FirstPage.tsx
+++ src/pages/FormPages/FirstPage.tsx
@@ -3,7 +3,17 @@ import { Layout } from '~/components';
 const FirstPage = () => {
   return (
     <Layout page={1}>
-      <div>FirstPage</div>
+      <div className='flex justify-between'>
+        <div></div>
+        <div className='relative'>
+          <img
+            src='/assets/icons/first-image.png'
+            alt='scan'
+            className='mt-[-92px]'
+          />
+          <div className='absolute animate-yellow-move bg-lime-350 opacity-70' />
+        </div>
+      </div>
     </Layout>
   );
 };
--- src/pages/FormPages/SecondPage.tsx
+++ src/pages/FormPages/SecondPage.tsx
@@ -3,7 +3,17 @@ import { Layout } from '~/components';
 const SecondPage = () => {
   return (
     <Layout page={2}>
-      <div>SecondPage</div>
+      <div className='flex justify-between'>
+        <div></div>
+        <div className='relative'>
+          <img
+            src='/assets/icons/second-image.png'
+            alt='scan'
+            className='mt-[-73px]'
+          />
+          <div className='absolute animate-red-move bg-lime-350 opacity-70 ' />
+        </div>
+      </div>
     </Layout>
   );
 };
