08aee324950cb628c1e086a39a9e60ec6a5fe8cf
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-12-21 19:47:25 +0400

    feat: create ability to send form data

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "src"),
+      '~': path.resolve(__dirname, 'src'),
     },
   },
   plugins: [react()],
+  define: {
+    'process.env.REACT_APP_API_URL': `"${process.env.REACT_APP_API_URL}"`,
+  },
 });
