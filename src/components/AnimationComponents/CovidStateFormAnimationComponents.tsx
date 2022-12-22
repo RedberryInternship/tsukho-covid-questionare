356f70ab9a2d328c4813df2cf919a658e6d8ab7e
giorgi <gcuxishvili@gmail.com>
2022-12-22 15:51:08 +0400

    fix: place green guy over red dot

--- src/components/AnimationComponents/CovidStateFormAnimationComponents.tsx
+++ src/components/AnimationComponents/CovidStateFormAnimationComponents.tsx
@@ -4,18 +4,18 @@ const CovidStateFormAnimationComponents = () => {
   const startingPoint = useQuery();
 
   return (
-    <div className='relative'>
-      <img
-        src='/assets/icons/second-image.png'
-        alt='scan'
-        className='mt-[-73px]'
-      />
+    <div className='relative w-[950px] h-[867px] '>
       <div
         className={`absolute ${
           startingPoint === 'forward'
             ? 'animate-red-move'
             : 'animate-red-move-backward'
-        } bg-lime-350 opacity-70 `}
+        } bg-lime-350 opacity-70 z-[1]`}
+      />
+      <img
+        src='/assets/icons/second-image.png'
+        alt='scan'
+        className='mt-[-73px] z-10 absolute'
       />
     </div>
   );
